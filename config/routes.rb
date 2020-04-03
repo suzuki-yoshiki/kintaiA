d0688fda8cdeae49f5e9f066af4e4a1b2aae0789
suzuki-yoshiki <yoshisu19950418@yahoo.co.jp>
2020-04-03 09:14:24 +0000

    login-function

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   root 'staticpages#top'
   get '/signup', to: 'users#new'
   
+  
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  
   resources :users
 end
